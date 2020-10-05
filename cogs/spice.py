8a770a86721f068ec59e7492de66bf67fe67ffea
Michael Koch <tastysauce@gmail.com>
2020-10-05 13:31:23 -0700

    removed global capitalization from instance vars

--- cogs/spice.py
+++ cogs/spice.py
@@ -5,15 +5,15 @@ from discord.ext import commands
 
 class SpiceCog(commands.Cog, name="Spice"):
 
-	SPICE_DELTA = 0.5
-	SPICE_TERRIBLE_DELTA = 1.25
-	SPICE_LEVEL = 0 #initially 0 and incremented when someone trips the array
-	SPICE_DECREMENT_INTERVAL = 300 # 5 minutes in seconds
+	spiceDeltaBadWords = 0.5
+	spiceDeltaTerribleWords = 1.25
+	spiceLevel = 0 #initially 0 and incremented when someone trips the array
+	spiceDecrementInterval = 300 # 5 minutes in seconds
 
-	SPICE_CHAMPS = {}
-	ARRAY_OF_TERRIBLE_WORDS = ["1080p", "1440p", "1080", "1440", "144hz", "144", "90", "90hz", "clown", ":90hz:", "fake", "blm", "trump", "antifa", "alt f4", "altf4", "f4", "intel", "nvidia", "amd"]
-	ARRAY_OF_BAD_WORDS = ["anal","anus","arse","ass","ballsack","balls","bastard","bitch","biatch","bloody","blowjob","blow" "job","bollock","bollok","boner","butt","buttplug","clitoris","cock","coon","crap","cunt","damn","dick","dildo","dyke","fag","feck","fellate","fellatio","felching","fuck","fudgepacker","fudge" "packer","flange","Goddamn","hell","homo","jerk","jizz","knobend","knob" "end","muff","nigger","nigga","omg","penis","piss","poop","prick","pube","pussy","queer","scrotum","sex","shit","sh1t","slut","smegma","spunk","tit","tosser","turd","twat","vagina","wank","whore"]
-	SPICE_CHAMP_TO_MUTE = ""
+	spiceChamps = {}
+	terribleWords = ["1080p", "1440p", "1080", "1440", "144hz", "144", "90", "90hz", "clown", ":90hz:", "fake", "blm", "trump", "antifa", "alt f4", "altf4", "f4", "intel", "nvidia", "amd"]
+	badWords = ["anal","anus","arse","ass","ballsack","balls","bastard","bitch","biatch","bloody","blowjob","blow" "job","bollock","bollok","boner","butt","buttplug","clitoris","cock","coon","crap","cunt","damn","dick","dildo","dyke","fag","feck","fellate","fellatio","felching","fuck","fudgepacker","fudge" "packer","flange","Goddamn","hell","homo","jerk","jizz","knobend","knob" "end","muff","nigger","nigga","omg","penis","piss","poop","prick","pube","pussy","queer","scrotum","sex","shit","sh1t","slut","smegma","spunk","tit","tosser","turd","twat","vagina","wank","whore"]
+	spiceChampToMute = ""
 
 	def __init__(self, bot):
 		self.bot = bot
@@ -23,7 +23,7 @@ class SpiceCog(commands.Cog, name="Spice"):
 
 	@commands.Cog.listener()
 	async def on_ready(self):
-		task = self.bot.loop.create_task(self.periodicallyDecrementSpice(self.SPICE_DECREMENT_INTERVAL))
+		task = self.bot.loop.create_task(self.periodicallyDecrementSpice(self.spiceDecrementInterval))
 		await task
 
 	@commands.command(name="level")
@@ -32,8 +32,8 @@ class SpiceCog(commands.Cog, name="Spice"):
 		member = await commands.MemberConverter().convert(ctx, target)
 
 		levelToDisplay = 0
-		if member in self.SPICE_CHAMPS:
-			levelToDisplay = round(self.SPICE_CHAMPS[member])
+		if member in self.spiceChamps:
+			levelToDisplay = round(self.spiceChamps[member])
 
 		await ctx.channel.send(member.name + "'s spice level: " + self.spiceLevelToEmoji(levelToDisplay))
 
@@ -49,46 +49,46 @@ class SpiceCog(commands.Cog, name="Spice"):
 
 		levelToDisplay = 0
 
-		for badWord in self.ARRAY_OF_BAD_WORDS:
+		for badWord in self.badWords:
 			if badWord in message.content.split():
 
 				#increment spice level and record name of edgy spice champ
-				self.SPICE_LEVEL = self.SPICE_LEVEL + self.SPICE_DELTA
+				self.spiceLevel = self.spiceLevel + self.spiceDeltaBadWords
 
 				# maintain a max of 5 peppers
-				if self.SPICE_LEVEL > 5:
-					self.SPICE_LEVEL = 5
+				if self.spiceLevel > 5:
+					self.spiceLevel = 5
 
 				# record the value for the user or add an entry for the rising champ
-				if message.author in self.SPICE_CHAMPS:
-					newChampLevel = self.SPICE_CHAMPS[message.author] + self.SPICE_DELTA
-					self.SPICE_CHAMPS[message.author] = newChampLevel
+				if message.author in self.spiceChamps:
+					newChampLevel = self.spiceChamps[message.author] + self.spiceDeltaBadWords
+					self.spiceChamps[message.author] = newChampLevel
 					levelToDisplay = round(newChampLevel)
 				else:
-					self.SPICE_CHAMPS[message.author] = self.SPICE_DELTA
-					levelToDisplay = self.SPICE_DELTA
+					self.spiceChamps[message.author] = self.spiceDeltaBadWords
+					levelToDisplay = self.spiceDeltaBadWords
 				# only allow 1 change per message
 				break
 
 		# but what if it's a terrible word
-		for terribleWord in self.ARRAY_OF_TERRIBLE_WORDS:
+		for terribleWord in self.terribleWords:
 			if terribleWord in message.content.split():
 
 				#increment spice level and record name of edgy spice champ
-				self.SPICE_LEVEL = self.SPICE_LEVEL + self.SPICE_TERRIBLE_DELTA
+				self.spiceLevel = self.spiceLevel + self.spiceDeltaTerribleWords
 
 				# maintain a max of 5 peppers
-				if self.SPICE_LEVEL > 5:
-					self.SPICE_LEVEL = 5
+				if self.spiceLevel > 5:
+					self.spiceLevel = 5
 
 				# record the value for the user or add an entry for the rising champ
-				if message.author in self.SPICE_CHAMPS:
-					newChampLevel = self.SPICE_CHAMPS[message.author] + self.SPICE_TERRIBLE_DELTA
-					self.SPICE_CHAMPS[message.author] = newChampLevel
+				if message.author in self.spiceChamps:
+					newChampLevel = self.spiceChamps[message.author] + self.spiceDeltaTerribleWords
+					self.spiceChamps[message.author] = newChampLevel
 					levelToDisplay = round(newChampLevel)
 				else:
-					self.SPICE_CHAMPS[message.author] = self.SPICE_TERRIBLE_DELTA
-					levelToDisplay = self.SPICE_TERRIBLE_DELTA
+					self.spiceChamps[message.author] = self.spiceDeltaTerribleWords
+					levelToDisplay = self.spiceDeltaTerribleWords
 				# only allow 1 change per message
 				break
 
@@ -97,11 +97,11 @@ class SpiceCog(commands.Cog, name="Spice"):
 		if round(levelToDisplay) == 3:
 			await message.channel.send(message.author.name + " spice level: " + spiceLevelToEmoji(levelToDisplay))
 
-		if levelToDisplay >= 5 and self.SPICE_CHAMP_TO_MUTE == "":
+		if levelToDisplay >= 5 and self.spiceChampToMute == "":
 			await message.channel.send("MUTE THE SPICE CHAMP?? Type !yup to mute " + message.author.name)
-			self.SPICE_CHAMP_TO_MUTE = message.author
+			self.spiceChampToMute = message.author
 		else: 
-			self.SPICE_CHAMP_TO_MUTE = ""
+			self.spiceChampToMute = ""
 
 	def spiceLevelToEmoji(self, level: int):
 		currentLevel = 0
@@ -120,17 +120,17 @@ class SpiceCog(commands.Cog, name="Spice"):
 	async def periodicallyDecrementSpice(self, timeout):
 		while True:
 			await asyncio.sleep(timeout)
-			if self.SPICE_LEVEL > 0:
-				self.SPICE_LEVEL = self.SPICE_LEVEL - 1
-				for key in self.SPICE_CHAMPS.keys():
-					newLevel = self.SPICE_CHAMPS[key] - 1
+			if self.spiceLevel > 0:
+				self.spiceLevel = self.spiceLevel - 1
+				for key in self.spiceChamps.keys():
+					newLevel = self.spiceChamps[key] - 1
 					if newLevel < 0:
 						newLevel = 0
-					self.SPICE_CHAMPS[key] = newLevel
+					self.spiceChamps[key] = newLevel
 
-			if self.SPICE_LEVEL <= 0:
-				self.SPICE_LEVEL = 0
-				self.SPICE_CHAMPS = {}
+			if self.spiceLevel <= 0:
+				self.spiceLevel = 0
+				self.spiceChamps = {}
 
 def setup(bot):
 	bot.add_cog(SpiceCog(bot))
--- cogs/voting.py
+++ cogs/voting.py
@@ -7,7 +7,7 @@ class VotingCog(commands.Cog, name="Voting"):
 		self.bot = bot
 		print("Initialized Voting")
 
-	CURRENT_VOTES = {}
+	currentVotes = {}
 
 	@commands.command(name="vote")
 	async def voteVoice(self, ctx, target):
@@ -18,12 +18,12 @@ class VotingCog(commands.Cog, name="Voting"):
 		member = await commands.MemberConverter().convert(ctx, target)
 
 		# Check if there's already a vote for this channel
-		if channel in self.CURRENT_VOTES:
+		if channel in self.currentVotes:
 			await ctx.channel.send("There's already a vote for " + channel.name)
 			return
 
 		# add empty array of votes
-		self.CURRENT_VOTES[channel] = {}
+		self.currentVotes[channel] = {}
 		# Get the member count of that channel
 		channelMembersCount = len(channel.members)
 
@@ -32,46 +32,46 @@ class VotingCog(commands.Cog, name="Voting"):
 			return
 
 		requiredToMute = round(channelMembersCount / 2)
-		self.CURRENT_VOTES[channel]["REQUIRED_TO_MUTE"] = requiredToMute
-		self.CURRENT_VOTES[channel]["TARGET"] = member
+		self.currentVotes[channel]["REQUIRED_TO_MUTE"] = requiredToMute
+		self.currentVotes[channel]["TARGET"] = member
 
 		await ctx.channel.send("Mute " + member.name + " in " + channel.name + "? " + str(requiredToMute) + " votes required.")
 		await ctx.channel.send("Vote !yes or !no")
 
 		await asyncio.sleep(60)
-		if channel in self.CURRENT_VOTES:
+		if channel in self.currentVotes:
 			await ctx.channel.send("Vote to mute " + member.name + " timed out after 1 minute")
-			self.CURRENT_VOTES.pop(channel, None)
+			self.currentVotes.pop(channel, None)
 	 
 
 	@commands.command(name="yes")
 	async def voteYes(self, ctx):
 		channel = ctx.author.voice.channel
-		if channel in CURRENT_VOTES:
-			self.CURRENT_VOTES[channel][ctx.author] = "yes"
+		if channel in currentVotes:
+			self.currentVotes[channel][ctx.author] = "yes"
 			await checkVotes(ctx)
 
 	@commands.command(name="no")
 	async def voteYes(self, ctx):
 
 		channel = ctx.author.voice.channel
-		if channel in self.CURRENT_VOTES:
-			self.CURRENT_VOTES[channel][ctx.author] = "no"
+		if channel in self.currentVotes:
+			self.currentVotes[channel][ctx.author] = "no"
 			await checkVotes(ctx)
 
 	async def checkVotes(self, ctx):
 		channel = ctx.author.voice.channel
-		if channel in self.CURRENT_VOTES:
-			member = self.CURRENT_VOTES[channel]["TARGET"]
-			keys = self.CURRENT_VOTES[channel].keys()
-			if len(keys) < self.CURRENT_VOTES[channel]["REQUIRED_TO_MUTE"]:
+		if channel in self.currentVotes:
+			member = self.currentVotes[channel]["TARGET"]
+			keys = self.currentVotes[channel].keys()
+			if len(keys) < self.currentVotes[channel]["REQUIRED_TO_MUTE"]:
 				return
 			else:
 				yesVotes = 0
 				for key in keys:
-					if self.CURRENT_VOTES[channel][key] == "yes":
+					if self.currentVotes[channel][key] == "yes":
 						yesVotes = yesVotes + 1
-				if yesVotes >= self.CURRENT_VOTES[channel]["REQUIRED_TO_MUTE"]:
+				if yesVotes >= self.currentVotes[channel]["REQUIRED_TO_MUTE"]:
 					await ctx.send("Muting " + member.name + " for 2 minutes")
 					await member.edit(mute=True)
 					mutedRole = discord.utils.get(member.guild.roles, name="muted")
@@ -83,19 +83,19 @@ class VotingCog(commands.Cog, name="Voting"):
 					await member.edit(mute=False)
 					await member.remove_roles(mutedRole)
 					await ctx.send(member.name + " is free again!")
-					CURRENT_VOTES.pop(channel, None)
+					currentVotes.pop(channel, None)
 
 	# @commands.has_role("botmancer")
 	@commands.command(name="yup", help="mutes the champ")
 	async def muteTheChamp(self, ctx):
 
-		if self.SPICE_CHAMP_TO_MUTE == "":
+		if self.spiceChampToMute == "":
 			return
 
-		member = self.SPICE_CHAMP_TO_MUTE
+		member = self.spiceChampToMute
 		# clear out the champ to mute and reset their level
-		self.SPICE_CHAMP_TO_MUTE = ""
-		self.SPICE_CHAMPS[member] = 0
+		self.spiceChampToMute = ""
+		self.spiceChamps[member] = 0
 
 	 	# mute em
 		await ctx.send("Timing out " + member.name + " for 10 minutes")
