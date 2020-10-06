a5e76633b1ef4d2c91f2767e77ee2a2166f2ce31
Michael Koch <tastysauce@gmail.com>
2020-10-05 17:24:35 -0700

    shitty bot!

--- cogs/shittyCommands.py
+++ cogs/shittyCommands.py
@@ -1,7 +1,6 @@
 import discord
 import asyncio
-import emoji
-import csv
+import random
 from discord.ext import commands
 
 class ShittyCommands(commands.Cog, name="Shitty Commands"):
@@ -9,19 +8,66 @@ class ShittyCommands(commands.Cog, name="Shitty Commands"):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command(name="shittyMute", help="mutes the champs")
-	async def muteTarget(self, ctx, champ, duration):
-		await ctx.send("Timing out " + champ.name + " for 5 minutes")
-		mutedRole = discord.utils.get(champ.guild.roles, name="muted")
-		await champ.add_roles(mutedRole)
-		await asyncio.sleep(duration)
+	# list of members
+	shittyMutedUsers = []
 
-		await ctx.send(champ.name + " is very calm now")
-		await champ.remove_roles(mutedRole)
+	# member, target
+	shittyMutesInProgress = {}
 
-		self.spiceChamps.pop(champ, None)
-		self.champsToMute.remove(champ)
-		self.champsThatHaveBeenWarned.remove(champ)
+	@commands.command(name="shittymute", help="mutes the champs")
+	async def startShittyMute(self, ctx, target):
+
+		target = target.strip("<!@>")
+		target = await commands.MemberConverter().convert(ctx, target)
+
+		if target in self.shittyMutedUsers:
+			await ctx.send(target.name + " is already muted")
+			return
+
+		if ctx.message.author in self.shittyMutesInProgress.keys():
+			print("author already muting someone")
+			return
+
+		await ctx.send(ctx.author.name + ": Pick a # between 1 and 3")
+
+		# self.shittyMutesInProgress[ctx.author] = target
+
+		# Wait for response
+		def check(message: discord.Message):
+			return message.author == ctx.author and self.convertMessageToInt(message)
+
+		try:
+			guess = await self.bot.wait_for('message', check=check, timeout=30)
+		except asyncio.TimeoutError:
+			await ctx.author.channel.send(ctx.author.name + " never guessed!")
+			return
+
+		if self.determineSuccess(int(guess.content)):
+			await ctx.send("Success! Timing out " + target.name + " for 1 minute")
+			await self.muteTarget(ctx, target)
+		else:
+			await ctx.send(ctx.author.name + " accidentally muted themself for a minute!")
+			await self.muteTarget(ctx, ctx.message.author)
+
+
+	async def muteTarget(self, ctx, target):
+		mutedRole = discord.utils.get(target.guild.roles, name="muted")
+		await target.add_roles(mutedRole)
+		await asyncio.sleep(60)
+		await ctx.send(target.name + " has escaped shittymute")
+		await target.remove_roles(mutedRole)
+
+	def determineSuccess(self, value):
+		randomNumber = random.randint(1,3)
+		return value == randomNumber
+
+	def convertMessageToInt(self, message):
+		try:
+			int(message.content)
+			return True
+		except ValueError:
+			return False
+		
 
 def setup(bot):
 	bot.add_cog(ShittyCommands(bot))
